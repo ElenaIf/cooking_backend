3c2234ce20f8e67f0f47278135d143759f164bee
Elena <ivankina.es@gmail.com>
2021-05-07 12:10:03 +0300

    cancelled changes

--- src/Controller/HomeScreenController.php
+++ src/Controller/HomeScreenController.php
@@ -12,18 +12,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeScreenController extends AbstractController
 {
-
-//    /**
-//     * @Route("/home", name="home_screen", methods={"GET", "POST"})
-//     */
-//    public function home(Request $request): Response {
-//
-////        return new Response('<h1>Hello!<h1/>');
-//
-////        This is json encode
-//        return $this -> json(['message' => $request -> query -> get("page")]);
-//    }
-
     /**
      * @Route("/recipes/add", name="add_new_recipe", methods={"GET", "POST"})
      */
@@ -63,35 +51,10 @@ class HomeScreenController extends AbstractController
 
         return $this->json($response);
 
-//        return new Response("get all recipes");
 
     }
 
-//    /**
-//     * @Route("/other", methods={"GET", "POST"})
-//     */
-//    public function other() {
-//        return $this -> redirectToRoute(route: "home_screen");
-//    }
-//
-//
-//
-//    /**
-//     * @Route("/recipe/{id}", name="get_a_recipe", methods={"GET"})
-//     */
-//    public function recipe($id) {
-//        return $this -> json(['message' => "requesting recipe number " . $id]);
-//    }
-//
-//    /**
-//     * @Route("/recipesjson/all", name="get_all_recipes", methods={"GET"})
-//     */
-//    public function getAllRecipes(){
-//        $rootPath = $this -> getParameter("kernel.project_dir");
-//        $recipes = file_get_contents($rootPath."/resources/recipes.json");
-//        $decodeRecipes = json_decode($recipes, true);
-//        return $this -> json($decodeRecipes);
-//    }
+
 
 
 }
